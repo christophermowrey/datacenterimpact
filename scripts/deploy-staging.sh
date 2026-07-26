f273ba68bd16a186520c60b844ccca3da47752e4
Christopher Mowrey <christophermowrey@gmail.com>
2026-07-26 18:35:25 -0500

    Fix Lightsail compose working directory

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -5,6 +5,7 @@ app_dir="/opt/data-center-impact"
 env_file="/etc/data-center-impact/app.env"
 
 test -f "$env_file"
+cd "$app_dir"
 git -C "$app_dir" fetch origin main
 git -C "$app_dir" reset --hard origin/main
 docker compose --env-file "$env_file" --profile production up -d --build --remove-orphans
