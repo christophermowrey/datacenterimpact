d5abf7784262546ded10ce5c561538fbeeb1ddfb
Christopher Mowrey <christophermowrey@gmail.com>
2026-07-29 20:32:53 -0500

    Add map technology route switcher

--- app/map-technology/[technology]/page.tsx
+++ app/map-technology/[technology]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation'
 import Home from '@/app/page'
+import TechnologySwitcher from '@/components/TechnologySwitcher'
 import { mapTechnologies, type MapTechnologyKey } from '@/lib/map-technologies'
 
 export function generateStaticParams() {
@@ -9,5 +10,5 @@ export function generateStaticParams() {
 export default async function MapTechnologyHome({ params }: { params: Promise<{ technology: string }> }) {
   const { technology } = await params
   if (!(technology in mapTechnologies)) notFound()
-  return <Home technology={technology as Exclude<MapTechnologyKey, 'maplibre'>} />
+  return <><TechnologySwitcher active={technology as MapTechnologyKey} /><Home technology={technology as MapTechnologyKey} /></>
 }
